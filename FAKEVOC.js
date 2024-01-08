c047841cb4b0a13d57b943018f1a3aff0da524f6
Faizal Rifaldy <34760458+fr00t16@users.noreply.github.com>
2024-01-08 10:58:25 +0700

    Update FAKEVOC.js
    awikwok

--- FAKEVOC.js
+++ FAKEVOC.js
@@ -82,7 +82,7 @@ const Action = require("./libs/action");
 				logger(pinNewItem.err_msg);
 			}
 			await action.getSales();
-			await sleep(71000);
+			await randomSleep(63000, 73000);
 		} catch (error) {
 			logger(error);
 		}
