07fba26add82f605171f05abf009c1dd6c137b88
Faizal Rifaldy <34760458+fr00t16@users.noreply.github.com>
2024-01-08 10:59:33 +0700

    Update LIVESTREAM.js

--- LIVESTREAM.js
+++ LIVESTREAM.js
@@ -13,20 +13,16 @@ rl.question('Enter the RTMP link and Stream KEY: ', (rtmpLink) => {
         .inputOptions(['-stream_loop -1'])
         .videoCodec('libx264')
         .audioCodec('aac')
-        .outputOptions(['-preset veryfast', '-r 30', '-b:v 2000k']) // Sesuaikan FPS dan Frame Rate di line ini
-        .withOutputFormat('flv')
-        .output(`${rtmpLink}`)
-        .on('end', () => {
-          console.log('Stream ended');
-          rl.close();
-        })
-        .on('error', (err) => {
-          console.error('Error:', err);
-          rl.close();
-        })
-        .on('progress', (progress) => {
-          console.log('Frames: ' + progress.frames);
-        })
-        .run();
-      });
-});
+        .outputOptions([
+        '-preset', 'slow',
+        '-crf', '23',
+        '-pix_fmt', 'yuv420p',
+        '-profile:v', 'main',
+        '-level', '4.1',
+        '-b:v', '2500k',
+        '-r', '30',
+        '-g', '60',
+        '-b:a', '128k',
+        '-ac', '2',
+        '-ar', '44100',
+      ])
